635d9d47ec0e5e73c324436f0e16bf97e2106f14
vaishnavihemane <140583160+vaishnavihemane@users.noreply.github.com>
2024-04-05 12:13:21 +0530

    Add files via upload

---
+++ FrameworkInSelenium1/FrameworkInSelenium1/FrameworkInSelenium1/src/main/java/com/natwest/FrameworkInSelenium1Application.java
@@ -0,0 +1,13 @@
+package com.natwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkInSelenium1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkInSelenium1Application.class, args);
+	}
+
+}
