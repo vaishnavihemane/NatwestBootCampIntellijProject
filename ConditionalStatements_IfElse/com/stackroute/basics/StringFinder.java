83767d71e46f978989a0e0e0b7b5b32aef0481d3
vaishnavihemane <140583160+vaishnavihemane@users.noreply.github.com>
2024-04-05 11:52:55 +0530

    Add files via upload

---
+++ ConditionalStatements_IfElse/com/stackroute/basics/StringFinder.java
@@ -0,0 +1,66 @@
+package com.stackroute.basics;
+
+import java.util.Scanner;
+
+public class StringFinder {
+
+
+
+    //Scanner class
+    private static Scanner sc;
+    public static void main(String[] args) {
+
+        StringFinder stringFinder = new StringFinder();
+
+        sc = new Scanner(System.in);
+        //Get three strings from the user
+        String searchString = stringFinder.getInput();
+        String firstString = stringFinder.getInput();
+        String secondString = stringFinder.getInput();
+
+        stringFinder.displayResult(stringFinder.findString(searchString,firstString,secondString));
+
+        stringFinder.closeScanner();
+
+    }
+
+    public String getInput(){
+        return sc.nextLine();
+    }
+
+    public void displayResult(int result) {
+
+        //displays the result
+        if(result == 1){
+            System.out.println("Found as expected");
+        }
+        else if(result == -1){
+            System.out.println("Empty string or null");
+        }
+        else{
+            System.out.println("Not found");
+        }
+
+
+    }
+
+    public int findString(String searchString, String firstString, String secondString) {
+
+        if (searchString == null || firstString == null || secondString == null || searchString.isEmpty() || firstString.isEmpty() || secondString.isEmpty()){
+            return -1;
+        }
+        else if(searchString.startsWith(firstString) &&  searchString.endsWith(secondString)) {
+            return 1;
+        }
+         else{
+             return 0;
+         }
+
+    }
+
+    public void closeScanner() {
+        if (sc==null) {
+            sc.close();;
+        }
+    }
+}
