c2d3f7fc6a49f6866a03f8523f1696e4c234e144
vaishnavihemane <140583160+vaishnavihemane@users.noreply.github.com>
2024-04-05 13:31:24 +0530

    Add files via upload

---
+++ SpringCloudApiGateway/SpringCloudAPIGatway/SpringCloudAPIGatway/src/main/java/com/natwest/SpringCloudApiGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.natwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatwayApplication.class, args);
+	}
+
+}
